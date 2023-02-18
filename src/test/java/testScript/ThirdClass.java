13409386d1900ef4680326ef5d4a520cf5f1813d
Lenovo <Lenovo@DESKTOP-IKQQ20M>
2023-02-18 09:56:23 +0530

    second commit

--- src/test/java/testScript/ThirdClass.java
+++ src/test/java/testScript/ThirdClass.java
@@ -1,5 +1,31 @@
 package testScript;
 
-public class ThirdClass {
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import genericLibraries.BaseClass;
+
+public class ThirdClass extends  BaseClass {
+	
+    @Test
+    public void thirdtest() throws InterruptedException {
+    	SoftAssert soft=new SoftAssert();
+    	
+    	home.searchFor("core java for selenium");
+    	soft.assertEquals(coreJava.getPageHeader(), "CORE JAVA FOR SELENIUM");
+    	
+    	coreJava.clickcorejavaforseleniumlink();
+    	soft.assertEquals(javaVideo.getPageHeader(), "CORE JAVA FOR SELENIUM TRAINING");
+    	
+    	javaVideo.clickclosecookiesi();
+    	//web.switchToFrame(null);
+    	javaVideo.clickplayButton();
+    	Thread.sleep(3000);
+    	javaVideo.clickpauseButton();
+    	
+    	//web.swichBackFromFrame();
+    	javaVideo.clickaddtowishlist();
+    	soft.assertAll();
+    }
 
 }
